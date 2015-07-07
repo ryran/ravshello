b59e6182d67d72d323c0c5cd7ba5c8a74656734d
Ryan Sawhill Aroha <rsaw@redhat.com>
2015-07-07 18:13:01 -0400

    v1.7.2: #49 and #50 and #51

--- _version.py
+++ _version.py
@@ -2,5 +2,5 @@
 # Copyright 2015 Ravshello Authors
 # License: Apache License 2.0 (see LICENSE or http://apache.org/licenses/LICENSE-2.0.html)
 
-__version__ = '1.7.0'
-__date__    = '2015/07/01'
+__version__ = '1.7.2'
+__date__    = '2015/07/07'
