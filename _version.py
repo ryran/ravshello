a87cf6d1b6776459827c42dc5570dec6e503805d
Ryan Sawhill Aroha <rsaw@redhat.com>
2015-07-28 12:28:02 -0400

    fix /apps/<APP> print_def cmd #57

--- _version.py
+++ _version.py
@@ -2,5 +2,5 @@
 # Copyright 2015 Ravshello Authors
 # License: Apache License 2.0 (see LICENSE or http://apache.org/licenses/LICENSE-2.0.html)
 
-__version__ = '1.7.2'
-__date__    = '2015/07/07'
+__version__ = '1.7.3'
+__date__    = '2015/07/28'
